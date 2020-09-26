3386aae6fd9b2dd3c408373e0276c9c6c018d90e
Uzoma Nwokolo <uzo.nwokolo@gmail.com>
2020-09-26 18:55:13 -0500

    homework Sept 2, 2020

--- src/main/java/MyHomeworkQA/Hw0926.java
+++ src/main/java/MyHomeworkQA/Hw0926.java
@@ -1,7 +1,27 @@
 package MyHomeworkQA;
 
 public class Hw0926 {
+
+    public static void sum(int p1, int p2){
+        System.out.println(p1 + p2);
+    }
+
+    public static int sum2(int p1, int p2){
+        return p1 + p2;
+    }
+
+    public static boolean isEven(int x1){
+        return x1 % 2 == 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("This is my homework file.");
+        System.out.println("Question 1:");
+        sum(23,2);
+        sum(8,3);
+        sum(11, 32);
+        System.out.println("\nQuestion 2:");
+        System.out.print(sum2(12,9)+", "+sum2(89,27)+", "+sum2(19,42));
+        System.out.println("\n\nQuestion 3:");
+        System.out.print(isEven(9)+", "+isEven(26)+", "+isEven(42)+", "+isEven(77));
     }
 }
