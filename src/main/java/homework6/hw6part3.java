f2621c9d969dbdfcecc0ab45f3b59e157d116d7a
“Emotionmotion” <yury.arakelov@gmail.com>
2020-09-27 20:17:24 -0700

    Homework6

---
+++ src/main/java/homework6/hw6part3.java
@@ -0,0 +1,27 @@
+package homework6;
+// 3. Create function `isEven` - int takes an int as an argument and return true if it is even or false if it is odd
+//invoke it with 4 different params - print out results
+public class hw6part3 {
+    public static void main(String[] args) {
+
+        boolean res1 = isEven(2);
+        boolean res2 = isEven(5);
+        boolean res3 = isEven( 55);
+        boolean res4 = isEven(66);
+        System.out.println(res1);
+        System.out.println(res2);
+        System.out.println(res3);
+        System.out.println(res4);
+    }
+
+    public static boolean isEven(int num1) {
+
+        if (isEven(num1) % 2 == 0) {
+            return true;
+            if (isEven(num1) % 2 == 1) {
+                return false;
+
+
+            }}
+
+    }}}}}}
