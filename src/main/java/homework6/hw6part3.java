1ae29a5c09235fa0e21523c8c2404af899704e14
“Emotionmotion” <yury.arakelov@gmail.com>
2020-09-28 20:22:55 -0700

    Homework6

--- src/main/java/homework6/hw6part3.java
+++ src/main/java/homework6/hw6part3.java
@@ -3,25 +3,17 @@ package homework6;
 //invoke it with 4 different params - print out results
 public class hw6part3 {
     public static void main(String[] args) {
+        System.out.println(isEven(2));
+        System.out.println(isEven2(3));
+        System.out.println(isEven(28));
+        System.out.println(isEven2(99));
 
-        boolean res1 = isEven(2);
-        boolean res2 = isEven(5);
-        boolean res3 = isEven( 55);
-        boolean res4 = isEven(66);
-        System.out.println(res1);
-        System.out.println(res2);
-        System.out.println(res3);
-        System.out.println(res4);
     }
-
     public static boolean isEven(int num1) {
+       boolean result =  num1%2 == 0;
+        return result;}
+        public static boolean isEven2(int num1) {
+            boolean result = num1%2 == 1;
+            return result;
 
-        if (isEven(num1) % 2 == 0) {
-            return true;
-            if (isEven(num1) % 2 == 1) {
-                return false;
-
-
-            }}
-
-    }}}}}}
+        }}
