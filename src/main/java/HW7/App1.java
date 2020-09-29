9085dd9ec9d9af45f4979828c909f09982fb1b1c
Nick1966 <nvakoliouk@gmail.com>
2020-09-28 20:32:25 -0700

    HW7 - function 2

--- src/main/java/HW7/App1.java
+++ src/main/java/HW7/App1.java
@@ -2,6 +2,16 @@ package HW7;
 
 public class App1 {
     public static void main(String[] args) {
+        System.out.println(isOdd(11));
+    }
 
+    public static boolean isEven(int number) {
+        boolean result = number % 2 == 0;
+        return result;
+    }
+
+    public static boolean isOdd(int number) {
+        boolean result = number % 2 == 1;
+        return result;
     }
 }
