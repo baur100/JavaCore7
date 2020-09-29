9acc501755f3bc2cfa560e397f2f352aef9b586a
Nick1966 <nvakoliouk@gmail.com>
2020-09-28 18:44:26 -0700

    L6 - function2

--- src/main/java/HW6/App1.java
+++ src/main/java/HW6/App1.java
@@ -1,4 +1,17 @@
 package HW6;
 
 public class App1 {
+    public static void main(String[] args) {
+        int ss1 = sum(10, 10);
+        System.out.println(ss1);
+        int ss2 = sum(20, 20);
+        System.out.println(ss2);
+        int ss3 = sum(30, 30);
+        System.out.println(ss3);
+    }
+
+    public static int sum(int number1, int number2) {
+        int result = number1 + number2;
+        return result;
+    }
 }
