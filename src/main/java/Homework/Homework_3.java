831e5d5f88cb4981e94d0e7c3573dc75564a8a98
Anna <khoptynets.ann@gmail.com>
2020-09-29 19:35:04 -0600

    Sep29_Homework_3

---
+++ src/main/java/Homework/Homework_3.java
@@ -0,0 +1,17 @@
+package Homework;
+
+            //Create function `isEven` - int takes an int as an argument and return true if it is even or false if it is odd
+            //invoke it with 4 different params - print out results
+
+public class Homework_3 {
+        public static boolean isEven (int number){
+
+        return (8%4)!=0;
+
+    }
+    public static void main(String[] args) {
+            System.out.print(isEven(2) + "\n============");
+            System.out.print(isEven(0) + "\n============");
+            System.out.print(isEven(4));
+    }
+}
