0130b6595431c4cd229241696faea454cc1acf11
Irina <irina19933@gmail.com>
2020-09-29 23:03:59 -0400

    homeworkFl

--- src/main/java/homework6fl/Hm6fl.java
+++ src/main/java/homework6fl/Hm6fl.java
@@ -1,4 +1,4 @@
-package homework6;
+package homework6fl;
 
 public class Hw6 {
     public static void main(String[] args) {
--- src/main/java/homeworkFl/homework6.java
+++
@@ -1,7 +0,0 @@
-package homeworkFl;
-
-public class Hw6 {
-    public static void main(String[] args) {
-        System.out.println("HW6+");
-    }
-}
