b76bbd55836dd4d4eee806873b8ad60e87e472a8
dmoreno-stack <daniela.jose.27@gmail.com>
2020-09-30 20:15:11 -0700

    lesson 9 - notes

---
+++ src/main/java/notes9/notesOOP.java
@@ -0,0 +1,36 @@
+package notes9;
+
+public class notesOOP {
+    public static void main(String[] args) {
+        //today working with object
+        //start by creating a person. we need to des
+        //class has methods & fields inside
+        //class is only description of object
+        //new objectstructure / initialization -> className = new className();
+        //create a real obj just initialize it-> className = new className();
+        //functions & methods start with minusculas
+        ex: class person(
+                sting int year; -> filed
+
+                public static drive(){} -> method
+
+
+        // giving an action
+        //since we get stuff from other files no longer use static
+        // ex: in lesson 9 ->public void introduce(){}
+
+        //f-field ; m-method;
+
+
+        //new object-> className = new className();
+
+
+        //NAME CONVENTION
+        //variable- lowercase
+        // class-uppercase (string included)
+        //functions-lowercase
+        //
+        )
+    }
+    public class person{}
+}
