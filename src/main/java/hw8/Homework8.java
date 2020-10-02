2fbc3067bf31d01d95c41dd38be8a86725fb310b
murat <muratj.tm@gmail.com>
2020-10-02 17:17:49 -0400

    Homework 8 Array sum sort minimum

--- src/main/java/hw8/Homework8.java
+++ src/main/java/hw8/Homework8.java
@@ -1,8 +1,5 @@
 package hw8;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class Homework8 {
     public static void main(String[] args) {
         int[] arr = {2,3,5,6,7,8};
