a1b0b7df0e068e03fbd732152a536dbb5c60ad73
irinaRiz <irinarizvanova160@gmail.com>
2020-10-02 16:06:57 -0400

    answer_for_lesson9

---
+++ src/main/java/Homework_for_lesson9/Applying1.java
@@ -0,0 +1,14 @@
+package Homework_for_lesson9;
+
+public class Applying1 {
+    public static void main(String[] args) {
+
+        Sport sport=new Sport();
+        sport.name="Football";
+        sport.fans=150000000;
+        sport.origin="England";
+
+        sport.getSportInfo();
+
+    }
+}
