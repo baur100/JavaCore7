98c29e663950807724a467cf3126b8d175a28784
irinaRiz <irinarizvanova160@gmail.com>
2020-10-02 16:38:54 -0400

    answer_for_lesson9

---
+++ src/main/java/Homework_for_lesson9/Applying3.java
@@ -0,0 +1,15 @@
+package Homework_for_lesson9;
+
+public class Applying3 {
+    public static void main(String[] args) {
+
+        Hobby myHobby=new Hobby();
+        myHobby.name="reading";
+        myHobby.period="childhood";
+        myHobby.features="develop imagination";
+
+        myHobby.describing();
+
+
+    }
+}
---
+++ src/main/java/Homework_for_lesson9/Hobby.java
@@ -0,0 +1,15 @@
+package Homework_for_lesson9;
+
+public class Hobby {
+    public String name;
+    public String period;
+    public String features;
+
+    public void describing(){
+        System.out.println("My favorite hobby is"+" "+ name+
+                " "+",I'm keen of"+" "+name+" "+"from "+
+                        " "+period+"."+" My hobby helps me to"+" "+features+".");
+
+    }
+
+}
