b459b59e1deb773350806f02d394b9f76b59df00
Nick1966 <nvakoliouk@gmail.com>
2020-10-03 00:16:15 -0700

    HW10

--- src/main/java/HW10/App.java
+++ src/main/java/HW10/App.java
@@ -7,4 +7,38 @@ package HW10;
 //   one instance created used default constructor - fill up fields using setter
 //   second object created using full constructor print object info using print-out method.
 public class App {
+    public static void main(String[] args) {
+
+        Client elena = new Client();
+        elena.setFirstName("Alenka");
+        elena.setLastName("Petrova");
+        elena.setdob(2000);
+
+        System.out.println(elena.getdob());
+        System.out.println(elena.getLastName());
+        System.out.println(elena.getFirstName());
+
+        Cat murka = new Cat();
+        murka.setName("Murka");
+        murka.setBreed("Siams");
+        murka.setSize(25);
+        murka.setAge(3);
+
+        System.out.println(murka.getName());
+        System.out.println(murka.getBreed());
+        System.out.println(murka.getSize());
+        System.out.println(murka.getAge());
+
+        Dog baron = new Dog();
+        baron.setName("Baron");
+        baron.setBreed("Rotveiler");
+        baron.setColor("Black");
+        baron.setAge(9);
+
+        System.out.println(baron.getName());
+        System.out.println(baron.getBreed());
+        System.out.println(baron.getColor());
+        System.out.println(baron.getAge());
+
+    }
 }
