360a042e80ea0e2d7c1aa701858a2cdd6a2c5d89
Anna Etlin <annaetlin@gmail.com>
2020-10-02 19:14:24 -0700

    AEHWlesson 8 update

--- src/main/java/AEHWLesson8/App2.java
+++ src/main/java/AEHWLesson8/App2.java
@@ -20,6 +20,8 @@ public class App2 {
         System.out.println(minValue);
 
     sort(arrayY);
+    sortA(arrayY);
+    bubbleSort(arrayY);
 
 
     }
@@ -27,7 +29,7 @@ public class App2 {
     public static int sum(int[] arr) {
         int sum = 0;
         for(int v: arr){
-            sum = sum+v;
+            sum = sum+v; // sum+=v;
         }
         return sum;
     }
@@ -54,6 +56,30 @@ public class App2 {
         }
         System.out.println(Arrays.toString(arr));
 
+    }
+
+
+    public static void sortA(int[] array){
+        Arrays.sort(array);
+        for (int v:array){
+            System.out.println(v);
+        }
+    }
+
+    public static void bubbleSort(int[] array){
+        int n = array.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (array[j] > array[j+1])
+                {
+                    // swap arr[j+1] and arr[j]
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+        for(int v: array){
+            System.out.println(v);
+        }
     }
 
 
