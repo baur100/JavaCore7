1f5003e51ddb846a38356fffffc53a13d9b417dc
rahilkhan <rahil_rahil@hotmail.com>
2020-10-04 11:34:13 -0700

    1. Create 4 classes with fields and methods 2. In the App class create main() and create 2 instances of each class, fill all fields invoke methods

---
+++ src/main/java/Lesson10Homework/Computers.java
@@ -0,0 +1,15 @@
+package Lesson10Homework;
+
+public class Computers {
+    private String name;
+    private String type;
+    private int price;
+    private String memory;
+    private int processorSpeed;
+
+        public void printComputers() {
+            System.out.println("Name is: "+ name + "\n type is  " + type + "\n price is " + price + " " + memory + " " + processorSpeed);
+
+        }
+    }
+
