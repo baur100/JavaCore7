f08154dc45671dd1ae299187366ed6c559a747d3
dmoreno-stack <daniela.jose.27@gmail.com>
2020-10-05 13:37:48 -0700

    hw4 -setup

--- src/main/java/hw4/homework4.java
+++ src/main/java/hw4/homework4.java
@@ -1,4 +1,18 @@
 package hw4;
 
 public class homework4 {
+    public static void main(String[] args) {
+//        1. Create 4 classes with fields (at least 3 fields) and methods (at least 1 method)
+//        each class has at least 2 constructors (default and full)
+//        create printClass()/ printInfo() method to print out all fields
+//        fields are private
+//        each field has getter and setter
+
+
+        
+//        2. In the App class create main() and create 2 instanses of each class;
+//        one instance created used default constructor - fill up fields using setter
+//        second object created using full constructor
+//        print object info using print-out method
+    }
 }
