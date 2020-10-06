d3ffded1ae326d0e4d3136aab25ccd00ecc997d8
irinaRiz <irinarizvanova160@gmail.com>
2020-10-05 23:47:49 -0400

    homework_for_lesson12

--- src/main/java/homework_for_lesson12/Inst.java
+++ src/main/java/homework_for_lesson12/Inst.java
@@ -2,8 +2,8 @@ package homework_for_lesson12;
 
 public class Inst {
     public static void main(String[] args) {
-        Oatmeal myfood = new Oatmeal("Quaker");
+        Oatmeal myFood = new Oatmeal("Quaker");
 
-        myfood.HealthyFood();
+        myFood.HealthyFood();
     }
 }
