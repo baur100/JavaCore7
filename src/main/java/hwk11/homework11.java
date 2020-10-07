cfc88d14a30e13c6d37f1fae04f1bd37b29f2529
dmoreno-stack <daniela.jose.27@gmail.com>
2020-10-07 13:18:52 -0700

    hw11 - started

---
+++ src/main/java/hwk11/homework11.java
@@ -0,0 +1,10 @@
+package hwk11;
+
+public class homework11 {
+    public static void main(String[] args) {
+        //Create an interface and 2 classes which implements it
+        //Create 2 objects of each class
+        //Assign one object to interface type
+    }
+}
+
---
+++ src/main/java/hwk11/homework11plus.java
@@ -0,0 +1,10 @@
+package hwk11;
+
+public class homework11plus {
+    public static void main(String[] args) {
+        //1. Create a super class and 3 child classes
+        //create 2 object of each class (including superclass - don't make it abstract)
+    }
+
+
+}
