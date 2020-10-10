436e00dea0c1a122a9fdd69c828736b849f39a30
Irina <irina19933@gmail.com>
2020-10-10 01:03:32 -0400

    homeworkFl

---
+++ src/main/java/test11/Computer.java
@@ -0,0 +1,14 @@
+package test11;
+
+public class Computer {
+    public static void main(String[] args) {
+        ComputerF myComputerF = new ComputerF();
+        myComputerF.monitor="Acer";
+        myComputerF.mouse="Logitech";
+        myComputerF.processor="a Macbook";
+
+        myComputerF.setup();
+
+
+    }
+}
---
+++ src/main/java/test11/ComputerF.java
@@ -0,0 +1,12 @@
+package test11;
+
+public class ComputerF {
+    public String processor;
+    public String monitor;
+    public String mouse;
+
+
+    public void setup() {
+        System.out.println("I use "+processor+" as a processor, with "+monitor+" monitor and "+mouse+" mouse.");
+    }
+}
