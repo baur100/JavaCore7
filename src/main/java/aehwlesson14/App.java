1af109e4e72b816fbdc908d764c4051c2c088ea8
Anna Etlin <annaetlin@gmail.com>
2020-10-11 18:54:49 -0700

    aelesson 14 & 15

---
+++ src/main/java/aehwlesson14/App.java
@@ -0,0 +1,11 @@
+package aehwlesson14;
+
+public class App {
+    public static void main(String[] args) {
+        //Create 3 ArrayList - String, Integer and some Custom class
+        //Add 4+ elements to each List
+        //exercise methods add/set/remove/get/foreach and etc
+
+
+    }
+}
