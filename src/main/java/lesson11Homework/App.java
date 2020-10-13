10b8b1fa1f804636aa1d23e3d67ad46a3866122f
alghweire <60858707+alghweire@users.noreply.github.com>
2020-10-12 21:09:43 -0700

    Create a super class and 3 child classes : create 2 object of each class (including superclass - don't make it abstract)

--- src/main/java/lesson11Homework/App.java
+++ src/main/java/lesson11Homework/App.java
@@ -1,37 +1,48 @@
 package lesson11Homework;
 
+
+
 public class App {
 
     public static void main ( String[]args){
 
-
         BuildComputer myComputer1 = new BuildComputer("Desktop","DELL",800);
         myComputer1.printComputer();
 
+        GraphicsCard myGraphicsCard1 = new GraphicsCard(" MDX 1400","Geforce",300,5);
+        myGraphicsCard1.printGraphicsCard();
+
+        HardDrive myHardDrive1 = new HardDrive("GTX 1050 ","Geforce", 400);
+        myHardDrive1.printHardDrive();
+
+        Keyboard myKeyboard1 = new Keyboard("Wired","Logitec ", 50);
+        myKeyboard1.printKeyboard();
+
+        Mouse myMouse1 = new Mouse("Wired","Logitec", 50);
+        myMouse1.printMouse();
+
+        System.out.println("---------------------------------------------------------------------");
+
+     //--------------------------------------------------------------------------------------------------------------//
 
         BuildComputer myComputer2 = new BuildComputer("Laptop","Apple",750);
         myComputer2.printComputer();
 
 
-        HardDrive myHardDrive1 = new HardDrive("GTX 1050 ","Geforce", 300);
-        myHardDrive1.printHardDrive();
+        GraphicsCard myGraphicsCard2 = new GraphicsCard(" RC 450","Intel",400,8);
+        myGraphicsCard2.printGraphicsCard();
+
 
         HardDrive myHardDrive2 = new HardDrive("RTX 2060 ","NVIDIA", 250);
         myHardDrive2.printHardDrive();
 
 
-        Keyboard myKeyboard1 = new Keyboard("Wired","Logitec ", 30);
-        myKeyboard1.printKeyboard();
-
-        Keyboard myKeyboard2 = new Keyboard("Wireless Bluetooth","Microsoft", 40);
+        Keyboard myKeyboard2 = new Keyboard("Wireless Bluetooth","Microsoft", 50);
         myKeyboard2.printKeyboard();
 
-        Mouse myMouse1 = new Mouse("Wired","Logitec", 30);
-        myMouse1.printMouse();
 
-        Mouse myMouse2 = new Mouse("Wireless Bluetooth","Microsoft", 25);
+        Mouse myMouse2 = new Mouse("Wireless Bluetooth","Microsoft", 50);
         myMouse2.printMouse();
-
 
     }
 }
