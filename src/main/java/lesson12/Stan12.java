25d07233b169fda7add4d28aa7cb011722940151
Goldwiss83 <stan_ruzh@gmail.com>
2020-10-13 22:41:18 -0700

    Homework lesson 12

--- src/main/java/lesson12/Stan12.java
+++ src/main/java/lesson12/Stan12.java
@@ -8,13 +8,15 @@ public class Stan12 {
         MacBookPro bookPro1 = new MacBookPro("15", "intel i7", "large size", true);
         MacBookPro bookPro2 = new MacBookPro("13", "intel i5", "medium size", true);
 
-        book1.macOsCompatibleApps();
-        bookPro1.macOsCompatibleApps();
+//        book1.macOsCompatibleApps();
+//        bookPro1.macOsCompatibleApps();
 
         Laptop book3 = new MacBookAir("13", "intel i3", "size", true);
         Laptop book4 = new MacBookAir("12", "intel i3", "small size", true);
 
         MacOS book5 = new MacBookAir("13","intel i3", "medium size", false);
+
+        book5.macOsCompatibleApps();
     }
 
 }
