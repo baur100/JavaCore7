9332070dc1972f2541a522f6bff1e3d4c0b13e6b
dmoreno-stack <daniela.jose.27@gmail.com>
2020-10-16 20:06:53 -0700

    notes lesson 16

---
+++ src/main/java/notes16Selenium/App.java
@@ -0,0 +1,38 @@
+package notes16Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://udemy.com");
+
+        WebElement searchFiled = driver.findElement(By.cssSelector("[name='q']"));
+        searchFiled.sendKeys("Java");
+        searchFiled.sendKeys(Keys.ENTER);
+
+        Thread.sleep(2000);
+
+        WebElement searchResult = driver.findElement(By.cssSelector("h1"));
+        String text = searchResult.getText();
+
+        if (text.equals("10,000 results for 'java'")){
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+            System.out.println("Actual value is = "+ text);
+        }
+
+        Thread.
+
+
+
+
+
+    }
+}
