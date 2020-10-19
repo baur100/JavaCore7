e615896015a1b3f71d78b8e390145a7615e6c8ae
“Emotionmotion” <yury.arakelov@gmail.com>
2020-10-18 23:38:22 -0700

    hmw

--- src/main/java/HW14/App1.java
+++ src/main/java/HW14/App1.java
@@ -16,12 +16,21 @@ public class App1 {
         names.add(Name.IPADPRO7);
         names.add(Name.MACBOOKPRO2020);
         names.add(Name.IWATCH6);
+        names.remove(0);
+        names.add(0, Name.IPHONE12);
+
 
         List<Double> price = new ArrayList<>();
         price.add(0, 999.00);
         price.add(1, 599.00);
+        price.set(1, 7999.99);
         price.add(2, 1599.00);
         price.add(3, 399.00);
+
+
+
+//        for (String key : capitals.keySet()){
+////            System.out.println(key);
 
         NewAppleProducts products = new NewAppleProducts(gadgets,names,price);
 
