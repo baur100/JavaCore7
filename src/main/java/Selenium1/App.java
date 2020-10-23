c7886211a470712177787ce25958ba019470bd9c
murat <muratj.tm@gmail.com>
2020-10-23 18:48:21 -0400

    Selenium1

---
+++ src/main/java/Selenium1/App.java
@@ -0,0 +1,41 @@
+package Selenium1;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner input = new Scanner(System.in);
+        String searchFor = input.nextLine();
+
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+
+        driver.get("https://google.com");
+
+        WebElement searchField = driver.findElement(By.cssSelector("input[title=Search]"));
+        searchField.sendKeys(searchFor);
+        searchField.sendKeys(Keys.ENTER);
+
+        List<WebElement> results = driver.findElements(By.cssSelector("#rso>[class=g] a cite"));
+        for (WebElement v : results) {
+            String text = v.getText();
+            int fistSpace = text.indexOf(" ");
+
+            if (fistSpace > 0) {
+                int i = fistSpace;
+                System.out.println(text.substring(0, i));
+            } else {
+                System.out.println(text);
+            }
+        }
+
+        Thread.sleep(5000);
+
+        driver.close();
+    }
+}
