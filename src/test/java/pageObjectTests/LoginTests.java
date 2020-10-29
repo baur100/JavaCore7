1bc9e749e7cd9f0c6976a16f2b16e0e0f6cca6e0
baur <baurzhan.zh@gmail.com>
2020-10-28 23:10:02 -0400

    JavaSelenium - Lesson 11 - Parametrization, RetryAnalyzer, SkipTest

--- src/test/java/pageObjectTests/LoginTests.java
+++ src/test/java/pageObjectTests/LoginTests.java
@@ -1,51 +1,54 @@
 package pageObjectTests;
 
+import listeners.RetryAnalyzer;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageObjects.LoginPage;
 import pageObjects.MainPage;
 
 public class LoginTests extends BaseTest{
-    @Test
+    int count=0;
+    @Test(retryAnalyzer = RetryAnalyzer.class)
     public void loginTest_CorrectCredentials_LoggedIn() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.open();
-        MainPage mainPage = loginPage.login("koeluser06@testpro.io","te$t$tudent");
+        MainPage mainPage = loginPage.login(username,password);
         Assert.assertTrue(mainPage.isMainPage());
+        Assert.assertEquals(count++,2);
     }
     @Test
     public void loginTest_WrongCredentials_ErrorFrameDisplayed() throws InterruptedException {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.open();
-        loginPage.login("koeluser06@testpro.io","wrongPassword");
+        loginPage.login(username,wrongPassword);
         Assert.assertTrue(loginPage.isError());
     }
     @Test
     public void loginTest_CorrectCredentials_LoggedIn1() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.open();
-        MainPage mainPage = loginPage.login("koeluser06@testpro.io","te$t$tudent");
+        MainPage mainPage = loginPage.login(username,password);
         Assert.assertTrue(mainPage.isMainPage());
     }
     @Test
     public void loginTest_WrongCredentials_ErrorFrameDisplayed1() throws InterruptedException {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.open();
-        loginPage.login("koeluser06@testpro.io","wrongPassword");
+        loginPage.login(username,wrongPassword);
         Assert.assertTrue(loginPage.isError());
     }
     @Test
     public void loginTest_CorrectCredentials_LoggedIn2() {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.open();
-        MainPage mainPage = loginPage.login("koeluser06@testpro.io","te$t$tudent");
+        MainPage mainPage = loginPage.login(username,password);
         Assert.assertTrue(mainPage.isMainPage());
     }
     @Test
     public void loginTest_WrongCredentials_ErrorFrameDisplayed2() throws InterruptedException {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.open();
-        loginPage.login("koeluser06@testpro.io","wrongPassword");
+        loginPage.login(username,wrongPassword);
         Assert.assertTrue(loginPage.isError());
     }
 }
