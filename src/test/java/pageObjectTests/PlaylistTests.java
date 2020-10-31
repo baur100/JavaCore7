0ea105ba2e6e84914a03c41ae1fa99b343541790
irinaRiz <irinarizvanova160@gmail.com>
2020-10-30 20:20:05 -0400

    homework_firstTest

--- src/test/java/pageObjectTests/PlaylistTests.java
+++ src/test/java/pageObjectTests/PlaylistTests.java
@@ -19,7 +19,7 @@ public class PlaylistTests extends BaseTest{
         Assert.assertTrue(isCreated);
     }
 
-    @Test(enabled=true)
+    @Test
     public void playlistTest_RenamePlaylist_PlaylistRenamed(){
         LoginPage loginPage = new LoginPage(driver);
         loginPage.open();
