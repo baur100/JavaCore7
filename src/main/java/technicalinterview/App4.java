134cd3ce12aafec268ebc2cdbd0e2902a91c330f
baur <baurzhan.zh@gmail.com>
2020-11-04 23:05:35 -0500

    Lesson 16

---
+++ src/main/java/technicalinterview/App4.java
@@ -0,0 +1,13 @@
+package technicalinterview;
+
+public class App4 {
+    public static void main(String[] args) {
+        int[]arr = {1,4,5,7,7,2,3,0};
+
+        boolean xx = anyReapets(arr);
+    }
+
+    private static boolean anyReapets(int[] arr) {
+        return true;
+    }
+}
