b6e03d9a58b08fcb93c133fa27dc7c6821b50c34
Vainel <vladimir.vainelovich@gmail.com>
2020-11-10 11:02:49 -0500

    Classwork 23

---
+++ src/test/java/listeners/Listeners.java
@@ -0,0 +1,50 @@
+package listeners;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import pageObjects.BasePage;
+import pageObjects.MainPage;
+
+public class Listeners implements ITestListener {
+    private static Logger logger = LogManager.getLogger(BasePage.class);
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+        logger.info("Test = " + iTestResult.getName() + " started");
+        System.out.println("Test = " + iTestResult.getName() + " started");
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        System.out.println("Success " + iTestResult.getMethod().getMethodName());
+
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        logger.fatal("Test " + iTestResult.getName() + " FAILED - Call 911");
+
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
+    }
+}
--- src/test/java/pageObjects/MainPage.java
+++ src/test/java/pageObjects/MainPage.java
@@ -1,18 +1,26 @@
 package pageObjects;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class MainPage extends BasePage{
+    Logger logger = LogManager.getLogger(MainPage.class);
     public MainPage(WebDriver driver) {
         super(driver);
+        logger.debug("Main page created");
+
     }
     private WebElement getLogoutButton() {
+        logger.debug("Trying to wait for logout button");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".fa.fa-sign-out.control")));
+        logger.debug("Logout Button found - returning");
         return driver.findElement(By.cssSelector(".fa.fa-sign-out.control"));
     }
     private void clickPlusButton(){
+        logger.debug("Trying to wait for logout button");
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='fa fa-plus-circle control create']")));
 //        fluentWait.until(x->x.findElement(By.xpath("//*[@class='fa fa-plus-circle control create']")));
 
@@ -21,6 +29,7 @@ public class MainPage extends BasePage{
                 driver.findElement(By.xpath("//*[@class='fa fa-plus-circle control create']")).click();
                 return;
             } catch (ElementClickInterceptedException ignoring){
+                logger.error("Button not found attempt " + i);
 
             }
         }
