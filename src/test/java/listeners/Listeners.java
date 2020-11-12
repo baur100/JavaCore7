e97024c45378e07b15f479bf4829447279e86771
murat <muratj.tm@gmail.com>
2020-11-11 20:52:27 -0500

    Screenshot of failure

---
+++ src/test/java/listeners/Listeners.java
@@ -0,0 +1,42 @@
+package listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listeners implements ITestListener {
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+        System.out.println(iTestResult.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        System.out.println(iTestResult.getStatus());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
+    }
+}
--- src/test/java/pageObjectsTests/BaseTest.java
+++ src/test/java/pageObjectsTests/BaseTest.java
@@ -2,11 +2,15 @@ package pageObjectsTests;
 
 import helpers.BrowserFabric;
 import helpers.Browser;
+import helpers.Screenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
@@ -23,8 +27,13 @@ public class BaseTest {
 //        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
     @AfterMethod
-    public void stopApp() throws InterruptedException {
-//        Thread.sleep(500);
-        driver.close();
+    public void stopApp(ITestResult iTestResult) {
+        if (iTestResult.getStatus() == iTestResult.FAILURE){
+            Date date = new Date();
+            SimpleDateFormat formatter = new SimpleDateFormat("_MM-dd-yyyy-HH:mm:ss");
+            String fileName = iTestResult.getName()+formatter.format(date);
+            Screenshot.get(driver, fileName);
+        }
+        driver.quit();
     }
 }
