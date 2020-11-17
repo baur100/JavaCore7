d9e16a51df29eebabdd52f2d91ccfeeed21731c1
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-11-17 13:45:51 -0500

    new push

--- src/main/java/technicalinterview/App4.java
+++ src/main/java/technicalinterview/App4.java
@@ -2,12 +2,22 @@ package technicalinterview;
 
 public class App4 {
     public static void main(String[] args) {
-        int[]arr = {1,4,5,7,7,2,3,0};
+        int[] arr = {1, 4, 5, 7, 2, 3, 0};
 
         boolean xx = anyReapets(arr);
     }
 
     private static boolean anyReapets(int[] arr) {
-        return true;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("found");
+                    return true;
+                }
+            }
+        }
+        System.out.println(" not found");
+
+        return false;
     }
 }
