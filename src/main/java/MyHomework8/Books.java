b79b277b204a5eae57021ab9f1815581c1932172
Zarinchos <sharakhmetovva@gmail.com>
2021-02-04 21:47:45 -0700

    Homework7Zvvv

---
+++ src/main/java/MyHomework8/Books.java
@@ -0,0 +1,13 @@
+package MyHomework8;
+
+public class Books {
+    public String name;
+    public String author;
+    public String  type;
+
+
+    public void MyFavBook(){
+        System.out.println("This is my favorite book"+" " + name + " " + " it's written by"+ " " + author);
+    }
+
+}
---
+++ src/main/java/MyHomework8/ingredients.java
@@ -0,0 +1,20 @@
+package MyHomework8;
+
+public class ingredients {
+    public  String name;
+    public String ingredient1;
+
+    public String ingredient2;
+
+    public String ingredient3;
+
+    public String ingredient4;
+    public String ingredient5;
+
+    public String ingredient6;
+
+    public void iLikeToEat(){
+        System.out.println("I like cooking" + " " + " " + name);
+    }
+
+}
